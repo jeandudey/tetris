5517f447652e7a05bcd2bd3d3cd6ffca1780d021
Jean Pierre Dudey <jeandudey@hotmail.com>
2015-07-27 15:53:48 -0430

    Feat(Tetris): Refactorize ArgumentsList to a new class.

--- src/Main.cc
+++ src/Main.cc
@@ -1,26 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
-typedef std::vector<std::string> ArgumentsList;
-
-void fill_arguments(ArgumentsList &args, int argc, const char **argv)
-{
-    for (int i = 0; i < argc; i++)
-        args[i] = std::string(argv[i]);
-}
-
-void print_arguments(const ArgumentsList &arguments)
-{
-    for (int i = 0; i < arguments.size(); i++)
-        std::cout << "arguments[" << i << "]: " << arguments[i] << std::endl;
-}
+#include "ArgumentsList.hpp"
 
 int main(int argc, const char **argv)
 {
-    ArgumentsList arguments(argc);
+    ArgumentsList arguments(argc, argv);
 
-    fill_arguments(arguments, argc, argv);
-    print_arguments(arguments);
+    std::cout << arguments[0] << std::endl;
     return 0;
 }
