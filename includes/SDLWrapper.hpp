56c39164f991dba3d4cf2d57115c78bb57083c09
Jean Pierre Dudey <jeandudey@hotmail.com>
2015-07-28 13:33:21 -0430

    Feat(SDLWrapper): Create wrappers for SDL_Window and SDL_Renderer.

--- includes/SDLWrapper.hpp
+++ includes/SDLWrapper.hpp
@@ -12,3 +12,31 @@ class SDL {
   SDL(Uint32 flags = SDL_INIT_EVERYTHING);
   ~SDL();
 };
+
+class SDLWindow {
+ public:
+  SDLWindow(const std::string &title, int x, int y, int w, int h, Uint32 flags = SDL_WINDOW_SHOWN);
+
+  SDL_Window *get();
+
+  void close();
+
+ private:
+  SDL_Window *window_;
+};
+
+class SDLRenderer {
+ public:
+  SDLRenderer(SDLWindow window, Uint32 flags = SDL_RENDERER_ACCELERATED);
+
+  SDL_Renderer *get();
+
+  void set_draw_color(Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+  void draw_rectangle(const SDL_Rect *rect);
+  void clear();
+
+  void destroy();
+
+ private:
+  SDL_Renderer *renderer_;
+};
