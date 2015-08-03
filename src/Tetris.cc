baf896da5b47eddf97cd84df8a7d27e348ec5f37
Jean Pierre Dudey <jeandudey@hotmail.com>
2015-08-03 11:20:04 -0430

    Feat(OpenGL): Adjust orthographic projection.

--- src/Tetris.cc
+++ src/Tetris.cc
@@ -1,17 +1,33 @@
 #include "Tetris.hpp"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+
 Tetris::Tetris(SDLWindow &window)
     : context_(window),
       window_(window),
       glew_(),
-      quad_(Rectangle2d::Position(0.25f, 0.25f),
-            Rectangle2d::Size(0.5f, 0.5f)),
+      quad_(Rectangle2d::Position(),
+            Rectangle2d::Size(50, 50)),
       running_(false)
 {
     context_.swap_interval(1);
 
+    glm::vec2 wsize(window_.width(), window_.height());
+
+    glViewport(0, 0, wsize.x, wsize.y);
+
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
+
+    //glOrtho(0.f, wsize.x, 0.f, wsize.y, 0.f, 1.f);
+
+    glm::mat4 ortho = glm::ortho(0.f, wsize.x, wsize.y, 0.f);
+
+    glMultMatrixf(glm::value_ptr(ortho));
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
