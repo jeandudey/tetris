abd1108a9875b3e503833625f5b5597345889d89
Jean Pierre Dudey <jeandudey@hotmail.com>
2015-08-04 12:24:57 -0430

    Feat(Playfield): Draw game playfield grid with points.

--- src/Playfield.cc
+++ src/Playfield.cc
@@ -3,7 +3,6 @@
 #include "Rectangle2d.hpp"
 
 #include <glm/glm.hpp>
-#include <iostream>
 
 Playfield::Playfield()
     : matrix_(10, 22, 0)
@@ -18,15 +17,28 @@ void Playfield::update()
 
 void Playfield::draw()
 {
+
+    glBegin(GL_POINTS);
+
+    glColor3f(1.f, 1.f, 1.f);
+
+    for (int x = 0; x < matrix_.width(); ++x) {
+        for (int y = 0; y < matrix_.height(); ++y) {
+            glVertex2f(x * 32, y * 32);
+        }
+    }
+
+    glEnd();
+
     for (int x = 0; x < matrix_.width(); x++) {
         for (int y = 0; y < matrix_.height(); y++) {
             if (matrix_(x, y) != 0) {
-                Rectangle2d block(Rectangle2d::Position(32 * x, 32 * y),
+                Rectangle2d quad(Rectangle2d::Position(32 * x, 32 * y),
                                   Rectangle2d::Size(32, 32));
 
-                block.color(1.f, 0.f, 0.f);
+                quad.color(1.f, 0.f, 0.f);
 
-                block.draw();
+                quad.draw();
             }
         }
     }
